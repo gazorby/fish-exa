7b2fb0c3c5cc2f917ca0db3d1fe24abca383ae2d
Gazorby <gazorby@pm.me>
2020-04-26 00:59:46 +0200

    🐛 fix: add args for l alias

--- conf.d/fish-exa.fish
+++ conf.d/fish-exa.fish
@@ -5,7 +5,7 @@ if not set -q EXA_LG_OPTIONS; set -gx EXA_LG_OPTIONS --git; end
 if not set -q EXA_LE_OPTIONS; set -gx EXA_LE_OPTIONS --extended; end
 if not set -q EXA_LT_OPTIONS; set -gx EXA_LT_OPTIONS --tree --level; end
 
-alias l 'exa'
+alias l 'exa $argv'
 alias la 'exa $EXA_STANDARD_OPTIONS $EXA_LA_OPTIONS $argv'
 alias ld 'exa $EXA_STANDARD_OPTIONS $EXA_LD_OPTIONS $argv'
 alias lg 'exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS $argv'
