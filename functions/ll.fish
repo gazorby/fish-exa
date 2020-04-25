4489a001494e84b7652d6d4ac30b14201f2336ff
Gazorby <gazorby@pm.me>
2020-04-26 00:49:47 +0200

    🐛 fix: don't ignore arguments

--- functions/ll.fish
+++ functions/ll.fish
@@ -1,7 +1,7 @@
 function ll -d "Use exa and it's --git option if in a git repo"
     if git rev-parse --is-inside-work-tree &>/dev/null
-        exa $EXA_STANDARD_OPTIONS {$EXA_LL_OPTIONS} --git
+        exa $EXA_STANDARD_OPTIONS {$EXA_LL_OPTIONS} --git $argv
     else
-        exa $EXA_STANDARD_OPTIONS {$EXA_LL_OPTIONS}
+        exa $EXA_STANDARD_OPTIONS {$EXA_LL_OPTIONS} $argv
     end
 end
