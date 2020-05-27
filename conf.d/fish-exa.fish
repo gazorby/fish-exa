4bb3879361d5acd2b900f336e3eeec49e6f27b3e
Gazorby <gazorby@pm.me>
2020-05-27 09:30:54 +0200

    🐛 fix: fix alias arguments

--- conf.d/fish-exa.fish
+++ conf.d/fish-exa.fish
@@ -6,11 +6,11 @@ if not set -q EXA_LE_OPTIONS; set -gx EXA_LE_OPTIONS "--extended"; end
 if not set -q EXA_LT_OPTIONS; set -gx EXA_LT_OPTIONS "--tree" "--level"; end
 
 alias l 'exa $argv'
-alias la 'exa $EXA_STANDARD_OPTIONS $EXA_LA_OPTIONS $argv'
-alias ld 'exa $EXA_STANDARD_OPTIONS $EXA_LD_OPTIONS $argv'
-alias lg 'exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS $argv'
-alias le 'exa $EXA_STANDARD_OPTIONS $EXA_LE_OPTIONS $argv'
-alias lt 'exa $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS $argv'
+alias la 'exa $EXA_STANDARD_OPTIONS $EXA_LA_OPTIONS'
+alias ld 'exa $EXA_STANDARD_OPTIONS $EXA_LD_OPTIONS'
+alias lg 'exa $EXA_STANDARD_OPTIONS $EXA_LG_OPTIONS'
+alias le 'exa $EXA_STANDARD_OPTIONS $EXA_LE_OPTIONS'
+alias lt 'exa $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS'
 
 set -l uninstall (basename (status -f) .fish)_uninstall
 
